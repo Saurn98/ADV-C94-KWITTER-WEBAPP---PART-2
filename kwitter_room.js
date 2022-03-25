f1a1d9c92bc59ae7736ecf22f95bd3637f42d9ec
Saurn98 <87410763+Saurn98@users.noreply.github.com>
2022-03-25 14:15:47 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_pKemNbZn5Z10WGF14RpS0cXC1p20p28",
+    authDomain: "lets-chat-web-app-25598.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-25598-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-25598",
+    storageBucket: "lets-chat-web-app-25598.appspot.com",
+    messagingSenderId: "120822202601",
+    appId: "1:120822202601:web:4bac2dd3922cadda34f078",
+    measurementId: "G-J1GVKJDNMJ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
